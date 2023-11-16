e3e1136f366c6f0d68f8addb9154e8afd6460239
Sahlar <sahlar.salehi@gmail.com>
2023-11-16 13:45:43 -0500

    Updated css to use react bootstrap. Also went back and restyled homepage/styles.css to account for it.

--- hoos-available/src/FindProfessor.js
+++ hoos-available/src/FindProfessor.js
@@ -1,105 +1,134 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 import './styles.css';
-
 const FindProfessor = () =>{
     return(
         <>
-            <meta charSet="UTF-8" />
-            <title>Homepage</title>
-            <link rel="stylesheet" href="hoos-available/src/styles.css" />
+            <head>
+                <meta charSet="UTF-8" />
+                <title>Homepage</title>
+                <link rel="stylesheet" href="./styles.css" />
+            </head>
+            <body>
             <div className="top-container">
                 <h1 className="professor-title">Find Professors By Department</h1>
             </div>
-            <div className="department-container">
-                <select>
-                    <option className="African American and African Studies">
-                        African American and African Studies
-                    </option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="American Sign Language Program">
-                        American Sign Language Program
-                    </option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="American Studies">American Studies</option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="Antropology">Antropology</option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="Art">Art</option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="Astronomy">Astronomy</option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="Biology">Biology</option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="Chemistry">Chemistry</option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="Classics">Classics</option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="Cognitive Science">Cognitive Science</option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="Creative Writing">Creative Writing</option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="Drama">Drama</option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="East Asian Languages">East Asian Languages</option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-                <select>
-                    <option className="Economics">Economics</option>
-                    <option className="Professor"> Professor 1 </option>
-                    <option className="Professor"> Professor 2</option>
-                    <option className="Professor"> Professor 3 </option>
-                </select>
-            </div>
+            </body>
+            <Accordion className="department-container">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>African American and African Studies</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>American Sign Language Program</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>American Studies</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                    <Accordion.Header>Anthropology</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="4">
+                    <Accordion.Header>Art</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="5">
+                    <Accordion.Header>Astronomy</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="6">
+                    <Accordion.Header>Biology</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="7">
+                    <Accordion.Header>Chemistry</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="8">
+                    <Accordion.Header>Classics</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="9">
+                    <Accordion.Header>Cognitive Science</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="10">
+                    <Accordion.Header>Creative Writing</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="11">
+                    <Accordion.Header>Drama</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="12">
+                    <Accordion.Header>East Asian Languages</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="13">
+                    <Accordion.Header>Economics</Accordion.Header>
+                    <Accordion.Body>
+                        <option className="Professor"> Professor 1 </option>
+                        <option className="Professor"> Professor 2</option>
+                        <option className="Professor"> Professor 3 </option>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+
         </>
     );
 }
